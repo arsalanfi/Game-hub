d9860acb72046d22f22a8412763915e81c089cf3
Arsalan <Arsalan.fili51@gmail.com>
2025-08-19 16:26:21 +0330

    initial responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorPalette="black">App</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <GridItem area="aside" bg="red" hideBelow={"lg"}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
