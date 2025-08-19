d456ba72f4a31a5459d97db5fb63b7a07e93f79f
Arsalan <Arsalan.fili51@gmail.com>
2025-08-19 16:51:09 +0330

    install to dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import favicon1 from "../assets/favicon1.ico";
+import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react";
+import { useColorMode } from "@/components/ui/color-mode";
+import { LuMoon, LuSun } from "react-icons/lu";
 
 const Navbar = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
       <Image src={favicon1} boxSize="60px" />
       <Text>Navbar</Text>
+      <ClientOnly fallback={<Skeleton boxSize="8" />}>
+        <IconButton onClick={toggleColorMode} variant="outline" size="sm">
+          {colorMode === "light" ? <LuSun /> : <LuMoon />}
+        </IconButton>
+      </ClientOnly>
     </HStack>
   );
 };
