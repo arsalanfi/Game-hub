b1539e23901d173ee9d4f0dac2fc4bb7520aa960
Arsalan <Arsalan.fili51@gmail.com>
2025-08-20 08:48:39 +0330

    Dispaly CriticScore

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import type { Game } from "@/hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatFormIcon from "./PlatFormIcon";
+import CriticSoure from "./CriticSoure";
 
 interface Props {
   game: Game;
@@ -12,9 +13,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatFormIcon
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent={"space-between"}>
+          <PlatFormIcon
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticSoure score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card.Root>
   );
